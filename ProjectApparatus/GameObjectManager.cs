f83d2deaa3c2e9186ed29098e77460c1d380b0c4
toaaa <toa@toaaa.de>
2023-12-29 17:22:27 +0100

    Update GameObjectManager.cs

--- ProjectApparatus/GameObjectManager.cs
+++ ProjectApparatus/GameObjectManager.cs
@@ -1,31 +1,37 @@
+using GameNetcodeStuff;
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using GameNetcodeStuff;
 using UnityEngine;
 
 public class GameObjectManager
 {
+
     public static GameObjectManager Instance
     {
         get
         {
-            if (GameObjectManager.instance == null)
+            if (instance == null)
             {
-                GameObjectManager.instance = new GameObjectManager();
+                instance = new GameObjectManager();
             }
-            return GameObjectManager.instance;
+            return instance;
         }
     }
+
+    public const float CollectionInterval = 3f;
 
     public List<GrabbableObject> items = new List<GrabbableObject>();
     public List<Landmine> landmines = new List<Landmine>();
     public List<Turret> turrets = new List<Turret>();
-    public List<DoorLock> door_locks = new List<DoorLock>();
-    public List<EntranceTeleport> entrance_doors = new List<EntranceTeleport>();
+    public List<DoorLock> doorLocks = new List<DoorLock>();
+    public List<EntranceTeleport> entranceTeleports = new List<EntranceTeleport>();
     public List<PlayerControllerB> players = new List<PlayerControllerB>();
     public List<EnemyAI> enemies = new List<EnemyAI>();
     public List<SteamValveHazard> steamValves = new List<SteamValveHazard>();
     public List<PlaceableShipObject> shipObjects = new List<PlaceableShipObject>();
+    public List<TerminalAccessibleObject> bigDoors = new List<TerminalAccessibleObject>();
+
     public PlayerControllerB localPlayer;
     public HangarShipDoor shipDoor;
     public ShipLights shipLights;
@@ -33,41 +39,68 @@ public class GameObjectManager
     public DepositItemsDesk itemsDesk;
     public TVScript tvScript;
 
-    public IEnumerator CollectObjects() // This is awful and I need to look into a better way of doing this
+    public IEnumerator CollectObjects()
     {
         while (true)
         {
-            items.Clear();
-            landmines.Clear();
-            turrets.Clear();
-            door_locks.Clear();
-            entrance_doors.Clear();
-            players.Clear();
-            enemies.Clear();
-            steamValves.Clear();
-            shipObjects.Clear();
+            InitializeReferences();
+            ClearLists();
 
-            items.AddRange(UnityEngine.Object.FindObjectsOfType<GrabbableObject>());
-            landmines.AddRange(UnityEngine.Object.FindObjectsOfType<Landmine>());
-            turrets.AddRange(UnityEngine.Object.FindObjectsOfType<Turret>());
-            door_locks.AddRange(UnityEngine.Object.FindObjectsOfType<DoorLock>());
-            entrance_doors.AddRange(UnityEngine.Object.FindObjectsOfType<EntranceTeleport>());
-            players.AddRange(UnityEngine.Object.FindObjectsOfType<PlayerControllerB>());
-            enemies.AddRange(UnityEngine.Object.FindObjectsOfType<EnemyAI>());
-            shipObjects.AddRange(UnityEngine.Object.FindObjectsOfType<PlaceableShipObject>());
-            steamValves.AddRange(UnityEngine.Object.FindObjectsOfType<SteamValveHazard>());
+            CollectObjectsOfType(items);
+            CollectObjectsOfType(landmines);
+            CollectObjectsOfType(turrets);
+            CollectObjectsOfType(doorLocks);
+            CollectObjectsOfType(entranceTeleports);
+            CollectObjectsOfType(players, p => !p.name.StartsWith("Player #"));
+            CollectObjectsOfType(enemies);
+            CollectObjectsOfType(steamValves);
+            CollectObjectsOfType(shipObjects);
+            bigDoors = FindObjectsOfType<TerminalAccessibleObject>(obj => obj.isBigDoor);
 
-            if (GameNetworkManager.Instance)
-                localPlayer =  GameNetworkManager.Instance.localPlayerController;
-            shipLights = UnityEngine.Object.FindObjectOfType<ShipLights>();
-            shipTerminal = UnityEngine.Object.FindObjectOfType<Terminal>();
-            itemsDesk = UnityEngine.Object.FindObjectOfType<DepositItemsDesk>();
-            shipDoor = UnityEngine.Object.FindObjectOfType<HangarShipDoor>();
-            tvScript = UnityEngine.Object.FindObjectOfType<TVScript>();
-
-            yield return new WaitForSeconds(3f);
+            yield return new WaitForSeconds(CollectionInterval);
         }
     }
 
-    private static GameObjectManager instance;
+    public void InitializeReferences()
+    {
+        localPlayer = GameNetworkManager.Instance?.localPlayerController;
+        shipLights = UnityEngine.Object.FindObjectOfType<ShipLights>();
+        shipTerminal = UnityEngine.Object.FindObjectOfType<Terminal>();
+        itemsDesk = UnityEngine.Object.FindObjectOfType<DepositItemsDesk>();
+        shipDoor = UnityEngine.Object.FindObjectOfType<HangarShipDoor>();
+        tvScript = UnityEngine.Object.FindObjectOfType<TVScript>();
+    }
+
+    public void ClearLists()
+    {
+        items.Clear();
+        landmines.Clear();
+        turrets.Clear();
+        doorLocks.Clear();
+        entranceTeleports.Clear();
+        players.Clear();
+        enemies.Clear();
+        steamValves.Clear();
+        shipObjects.Clear();
+        bigDoors.Clear();
+    }
+
+    public void CollectObjectsOfType<T>(List<T> list, Predicate<T> predicate = null) where T : MonoBehaviour
+    {
+        foreach (var obj in UnityEngine.Object.FindObjectsOfType<T>())
+        {
+            if (predicate == null || predicate(obj))
+            {
+                list.Add(obj);
+            }
+        }
+    }
+
+    public List<T> FindObjectsOfType<T>(Predicate<T> predicate = null) where T : MonoBehaviour
+    {
+        var objects = UnityEngine.Object.FindObjectsOfType<T>();
+        return predicate == null ? new List<T>(objects) : new List<T>(objects).FindAll(predicate);
+    }
+
+    public static GameObjectManager instance;
 }
