c26e41bdf71b13719408347c3390b8f87767084b
toaaa <toa@toaaa.de>
2023-12-31 00:34:05 +0100

    Improved GetLootColor

--- ProjectApparatus/Hacks.cs
+++ ProjectApparatus/Hacks.cs
@@ -513,14 +513,10 @@ namespace ProjectApparatus
 
         private Color GetLootColor(int value)
         {
-            int[] thresholds = { 15, 35 };
-            Color[] colors = { settingsData.c_smallLoot, settingsData.c_medLoot, settingsData.c_bigLoot };
-
-            for (int i = 0; i < thresholds.Length; i++)
-                if (value <= thresholds[i])
-                    return colors[i];
-
-            return settingsData.c_Loot;
+            if (value <= 15) return settingsData.c_smallLoot;
+            if (value > 15 && value <= 35) return settingsData.c_medLoot;
+            if (value >= 36) return settingsData.c_bigLoot;
+            else return settingsData.c_Loot;
         }
 
         private void DisplayLoot()
