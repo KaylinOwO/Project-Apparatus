57abb379855c422460589ce379db8b2726ac9f9a
Jade <jadevain@outlook.com>
2024-01-05 13:06:41 +0300

    Add support language. Added language selection button in settings tab. Add 2 language dictionary RU and EN. Replaced all text with variables with text.

---
+++ ProjectApparatus/LocalizationManager.cs
@@ -0,0 +1,44 @@
+﻿using ProjectApparatus.Lang;
+using ProjectApparatus.Properties;
+using System;
+using System.Resources;
+
+public class LocalizationManager
+{
+    private static ResourceManager resourceManager;
+    public static string currentLanguage = "en_US";
+
+    static LocalizationManager()
+    {
+        SetLanguage(currentLanguage);
+    }
+
+    public static void SetLanguage(string language)
+    {
+        switch (language)
+        {
+            case "en_US":
+                resourceManager = new ResourceManager(typeof(en_US));
+                break;
+            case "ru_RU":
+                resourceManager = new ResourceManager(typeof(ru_RU));
+                break;
+            // add next language here
+            default:
+                throw new ArgumentException("Unsupported language: " + language);
+        }
+        currentLanguage = language;
+    }
+
+    public static string GetString(string key)
+    {
+        try
+        {
+            return resourceManager.GetString(key);
+        }
+        catch (Exception)
+        {
+            return "Missing translation for key: " + key;
+        }
+    }
+}
