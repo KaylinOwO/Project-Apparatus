f0818c7a5891f41738e6cf78ea6047ecd632be4e
TheGreenBandit <106003542+TheGreenBandit@users.noreply.github.com>
2024-01-07 21:39:24 -0500

    two things
    changed to use already declared instance for totalscrap in ship,
    added spawn enemy on player, currently random, could be changed to a certain type by changing the enemy type from -1

--- ProjectApparatus/Hacks.cs
+++ ProjectApparatus/Hacks.cs
@@ -57,15 +57,16 @@ namespace ProjectApparatus
 
             GUI.color = settingsData.c_Theme;
 
-            GameObject ship = GameObject.Find("/Environment/HangarShip");
-            var loot = ship.GetComponentsInChildren<GrabbableObject>().Where(
-                obj => obj.name != "ClipboardManual" && obj.name != "StickyNoteItem").ToList();
+            int scrapvaluetotal = 0;
+            foreach (GrabbableObject ob in Instance.items)
+                if (!ob.heldByPlayerOnServer && ob.isInShipRoom && ob.name != "ClipboardManual" && ob.name != "StickyNoteItem")
+                    scrapvaluetotal += ob.scrapValue;
 
             if (settingsData.b_CenteredIndicators)
             {
                 float iY = Settings.TEXT_HEIGHT;
                 if (settingsData.b_DisplayGroupCredits && Instance.shipTerminal != null) Render.String(Style, centeredPos.x, centeredPos.y + 7 + iY, 150f, Settings.TEXT_HEIGHT, "Group Credits: " + Instance.shipTerminal.groupCredits, GUI.color, true, true); iY += Settings.TEXT_HEIGHT - 10f;
-                if (settingsData.b_DisplayLootInShip && Instance.shipTerminal) Render.String(Style, centeredPos.x, centeredPos.y + 7 + iY, 150f, Settings.TEXT_HEIGHT, "Loot In Ship: " + loot.Sum(scrap => scrap.scrapValue), GUI.color, true, true); iY += Settings.TEXT_HEIGHT - 10f;
+                if (settingsData.b_DisplayLootInShip && Instance.shipTerminal) Render.String(Style, centeredPos.x, centeredPos.y + 7 + iY, 150f, Settings.TEXT_HEIGHT, "Loot In Ship: " + scrapvaluetotal, GUI.color, true, true); iY += Settings.TEXT_HEIGHT - 10f;
                 if (settingsData.b_DisplayQuota && TimeOfDay.Instance) Render.String(Style, centeredPos.x, centeredPos.y + 7 + iY, 150f, Settings.TEXT_HEIGHT, "Profit Quota: " + TimeOfDay.Instance.quotaFulfilled + "/" + TimeOfDay.Instance.profitQuota, GUI.color, true, true); iY += Settings.TEXT_HEIGHT - 10f;
                 if (settingsData.b_DisplayDaysLeft && TimeOfDay.Instance) Render.String(Style, centeredPos.x, centeredPos.y + 7 + iY, 150f, Settings.TEXT_HEIGHT, "Days Left: " + TimeOfDay.Instance.daysUntilDeadline, GUI.color, true, true); iY += Settings.TEXT_HEIGHT - 10f;
             }
@@ -78,7 +79,7 @@ namespace ProjectApparatus
                 if (settingsData.b_DisplayGroupCredits && Instance.shipTerminal != null)
                     Watermark += $" | Group Credits: {Instance.shipTerminal.groupCredits}";
                 if (settingsData.b_DisplayLootInShip && Instance.shipTerminal)
-                    Watermark += $" | Loot In Ship: {loot.Sum(scrap => scrap.scrapValue)}";
+                    Watermark += $" | Loot In Ship: {scrapvaluetotal}";
                 if (settingsData.b_DisplayQuota && TimeOfDay.Instance)
                     Watermark += $" | Profit Quota: {TimeOfDay.Instance.quotaFulfilled} / {TimeOfDay.Instance.profitQuota}";
                 if (settingsData.b_DisplayDaysLeft && TimeOfDay.Instance)
@@ -361,6 +362,7 @@ namespace ProjectApparatus
 
                     if (!selectedPlayer.isPlayerDead)
                     {
+                        UI.Button("Spawn Enemy", "Spawns a random enemy on the selected player.", () => { RoundManager.Instance.SpawnEnemyOnServer(selectedPlayer.gameplayCamera.transform.position, 50); });
                         UI.Button("Kill", "Kills the currently selected player.", () => { selectedPlayer.DamagePlayerFromOtherClientServerRpc(selectedPlayer.health + 1, new Vector3(900, 900, 900), 0); });
                         UI.Button("Teleport To", "Teleports you to the currently selected player.", () => { Instance.localPlayer.TeleportPlayer(selectedPlayer.playerGlobalHead.position); });
                         UI.Button("Teleport Enemies To", "Teleports all enemies to the currently selected player.", () =>
