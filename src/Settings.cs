c1e552e7eb61b18632b15a98e9c58e9592c41b5e
TheGreenBandit <106003542+TheGreenBandit@users.noreply.github.com>
2024-01-24 21:52:50 -0500

    revert

--- src/Settings.cs
+++ src/Settings.cs
@@ -10,7 +10,7 @@ namespace ProjectApparatus
     [System.Serializable]
     public class SettingsData
     {
-        public readonly string version = "1.2.1";
+        public readonly string version = "1.2.0";
 
         /* ESP */
         public bool b_EnableESP;
